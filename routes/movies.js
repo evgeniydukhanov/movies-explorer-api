f5fa77935fa0fe73e9d3f1036d1a32b6ec28ce3a
evgeniydukhanov <duhanoves@itwis.org>
2022-05-28 21:45:42 +0700

    validation change

--- routes/movies.js
+++ routes/movies.js
@@ -31,7 +31,7 @@ router.delete(
   '/:movieId',
   celebrate({
     params: Joi.object().keys({
-      movieId: Joi.string().length().hex().required(),
+      movieId: Joi.string().length(24).hex().required(),
     }),
   }),
   deleteMovie,
