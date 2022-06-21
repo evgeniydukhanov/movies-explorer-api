1259b235bb4316487ce4eec80fd7e4b404149205
evgeniydukhanov <duhanoves@itwis.org>
2022-06-21 22:39:37 +0700

    changed limiter

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -1,9 +1,9 @@
 const rateLimit = require('express-rate-limit');
 
 module.exports = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 минутное окно
-  max: 300,
+  windowMs: 2 * 60 * 1000, // 15 минутное окно
+  max: 200,
   delayMs: 0,
-  message: 'Вы сделали слишком много запросов за 15 минут',
+  message: 'Вы сделали слишком много запросов за 2 минуты',
   headers: true,
 });
